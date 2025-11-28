22c0473dbaa2be9faec83dffc0e79f9edef81d36
LotusReichhart <reichhartlotus@gmail.com>
2025-11-29 02:21:53 +0700

    chore(build): add AndroidFeatureConventionPlugin to build logic

---
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,33 @@
+import com.android.build.api.dsl.LibraryExtension
+import com.lotusreichhart.gencanvas.configureAndroidCompose
+import com.lotusreichhart.gencanvas.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.apply
+import org.gradle.kotlin.dsl.configure
+import org.gradle.kotlin.dsl.dependencies
+
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            apply(plugin = "gencanvas.android.library")
+            apply(plugin = "gencanvas.android.hilt")
+
+            extensions.configure<LibraryExtension> {
+                testOptions.animationsDisabled = true
+                configureAndroidCompose(this)
+            }
+
+            dependencies {
+                "implementation"(project(":core:ui"))
+
+                "implementation"(libs.findLibrary("androidx.hilt.navigation.compose").get())
+                "implementation"(libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
+                "implementation"(libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
+                "implementation"(libs.findLibrary("androidx.navigation.compose").get())
+
+                "testImplementation"(libs.findLibrary("androidx.navigation.testing").get())
+            }
+        }
+    }
+}
