3561fa17ce95e2146f6e39f4e2707f59ea133490
LotusReichhart <reichhartlotus@gmail.com>
2025-12-03 19:59:44 +0700

    fix(data): error returning wrong message

--- core/data/src/main/kotlin/com/lotusreichhart/gencanvas/core/data/repository/UserRepositoryImpl.kt
+++ core/data/src/main/kotlin/com/lotusreichhart/gencanvas/core/data/repository/UserRepositoryImpl.kt
@@ -52,7 +52,6 @@ class UserRepositoryImpl @Inject constructor(
                 }
             } catch (e: Exception) {
                 Timber.e(e, "Lưu RoomDB thất bại")
-                return Result.failure(e)
             }
         }
 
